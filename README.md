0238a79e885733d31c0c4ebc5064213a41df91a8
UPMCYatte <65915835+UPMCYatte@users.noreply.github.com>
2023-07-08 15:16:48 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+npm init -y
+npm install --save-dev http-server
+npx http-server
---
+++ script.js
@@ -0,0 +1,35 @@
+import { directions, keys } from './constants.js'
+import Sokoban from './Sokoban.js'
+
+const sokoban = new Sokoban({ level: 1 })
+sokoban.render({ restart: true })
+
+document.addEventListener('keydown', (event) => {
+  const playerCoords = sokoban.findPlayerCoords()
+
+  switch (event.key) {
+    case keys.up:
+    case keys.w:
+      sokoban.move(playerCoords, directions.up)
+      break
+    case keys.down:
+    case keys.s:
+      sokoban.move(playerCoords, directions.down)
+      break
+    case keys.left:
+    case keys.a:
+      sokoban.move(playerCoords, directions.left)
+      break
+    case keys.right:
+    case keys.d:
+      sokoban.move(playerCoords, directions.right)
+      break
+    default:
+  }
+
+  sokoban.render()
+})
+
+document.querySelector('button').addEventListener('click', (event) => {
+  sokoban.render({ restart: true })
+})
